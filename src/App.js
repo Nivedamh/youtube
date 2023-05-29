13fc150985d694687454d46106cc66c041c178ff
Nivedamh <niveda1012@gmail.com>
2023-05-29 17:20:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';// when it is in same folder give./and file name if it is in different folder ../filename
+
+
+function App() {
+ return (
+<div className='App'>
+<h1>you-tube</h1>
+</div>
+ );
+}
+export default App;
