d4419140eb4c622e4a447d530d300b154f7268fb
Nivedamh <niveda1012@gmail.com>
2023-05-29 17:46:11 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';// when it is in same folder give./and file name if it is in different folder ../filename
+import Navbar from './components/navbar'
 
-
-function App() {
- return (
-<div className='App'>
-<h1>you-tube</h1>
-</div>
- );
+function App(){
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
 }
-export default App;
+export default App; 
